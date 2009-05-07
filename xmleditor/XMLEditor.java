68b39b915d7c88fc3475a4a0e4305de75daadcf5
Christian Tietze <christian.tietze@googlemail.com>
2009-05-07 20:42:24 +0200

    initial commit

---
+++ xmleditor/XMLEditor.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package xmleditor;
+
+/**
+ * Provides a program starting point. <code>XMLEditor</code> initializes
+ * the GUI.
+ * 
+ * @author Christian Tietze
+ */
+public class XMLEditor {
+
+	/**
+	 * To be run on program execution.
+	 * 
+	 * @param args Command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO Create Window instance
+
+	}
+
+}

a69c2c0dc81971c1a969156d780d81347b56526e
Christian Tietze <christian.tietze@googlemail.com>
2009-05-08 17:29:08 +0200

    basic first setup

---
+++ xmleditor/gui/editor/EditorWindow.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package xmleditor.gui.editor;
+
+import java.awt.Frame;
+import java.awt.GraphicsConfiguration;
+import java.awt.Window;
+
+import javax.swing.JWindow;
+
+/**
+ * 
+ * @author Christian Tietze
+ */
+public class EditorWindow extends JWindow {
+
+	/**
+	 * 
+	 */
+	public EditorWindow() {
+		// TODO Auto-generated constructor stub
+	}
+}
